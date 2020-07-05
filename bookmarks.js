bf1d5e86b7f2e2c2e53e3e0528be59fb5846f347
Mark <sipizork@gmail.com>
2020-07-05 23:10:21 +0200

    create min-height to articles and header, modify the bookmarks for new heights

--- bookmarks.js
+++ bookmarks.js
@@ -4,22 +4,33 @@ const aboutMeBookmark = document.querySelector('.bookmarks .about__me');
 const referencesBookmark = document.querySelector('.bookmarks .references');
 const contactsBookmark = document.querySelector('.bookmarks .contacts');
 
+const header = document.querySelector('#page-header');
+const aboutMe = document.querySelector('#about__me');
+const references = document.querySelector('#references');
+const contacts = document.querySelector('#contacts');
+
+const headerHeight = header.clientHeight;
+const aboutMeHeight = aboutMe.clientHeight;
+const referencesHeight = references.clientHeight;
+const contactsHeight = contacts.clientHeight;
+
+
 window.addEventListener('scroll', () => addBookMarks())
 
 addBookMarks = () => {
-  if (window.scrollY > window.innerHeight - 100) {
+  if (window.scrollY > headerHeight - 100) {
     headerBookmark.classList.add('visible');
   } else {
     headerBookmark.classList.remove('visible');
   }
 
-  if (window.scrollY < window.innerHeight - 170 || window.scrollY > (window.innerHeight * 2) - 140 ) {
+  if (window.scrollY < headerHeight - 180 || window.scrollY > headerHeight + aboutMeHeight - 145 ) {
     aboutMeBookmark.classList.add('visible');
   } else {
     aboutMeBookmark.classList.remove('visible');
   }
 
-  if (window.scrollY < (window.innerHeight * 2) - 215 || window.scrollY > (window.innerHeight * 3) - 180 ) {
+  if (window.scrollY < headerHeight + aboutMeHeight - 220 || window.scrollY > headerHeight + aboutMeHeight + referencesHeight - 195 ) {
     referencesBookmark.classList.add('visible');
   } else {
     referencesBookmark.classList.remove('visible');
